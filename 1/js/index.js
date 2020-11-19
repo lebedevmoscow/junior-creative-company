38f01939a43385c0055df4e5b8331201704b90a1
Boris Lebedev <lebedev.moscow.me@gmail.com>
2020-11-19 14:55:53 +0500

    End with navbar

--- 1/js/index.js
+++ 1/js/index.js
@@ -1,2 +1,12 @@
 const burger = document.querySelector('.navbar__burger')
-burger.addEventListener('click', () => {})
+const navbar = document.querySelector('.navbar__list')
+
+burger.addEventListener('click', () => {
+	navbar.classList.toggle('navbar__list-opened')
+	console.log('click')
+	if (navbar.classList.contains('navbar__list-opened')) {
+		navbar.style.left = 0
+	} else {
+		navbar.style.left = '-30%'
+	}
+})
